ef49f49fe2f28580c751fd159aeef4228d2ecb7f
hardekarora <129650068+hardekarora@users.noreply.github.com>
2024-08-10 21:16:09 +0530

    Deleted extra  routes directory

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require ("express");
-const userRouter = require("./user")
-const  accountRouter = require("./accounts");
-const router = express.Router(); 
-router.use("/user",userRouter)
-router.use("/account" , accountRouter)
-
-module.exports = router;
